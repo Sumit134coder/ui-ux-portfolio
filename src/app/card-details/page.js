a5c0083daf6d160035c29952ce65d3d525aa428a
Sumit134Coder <mehrasumit356@gmail.com>
2023-06-29 11:28:19 +0530

    update , complete : styles

--- src/app/card-details/page.js
+++ src/app/card-details/page.js
@@ -1,54 +1,86 @@
-"use client"
+/* eslint-disable react/no-unescaped-entities */
+"use client";
 
-import './index.css'
-import { useState } from 'react'
-import Image from 'next/image'
-import cardLogo from '../../../public/card/card-logo.svg'
-import cardIco from '../../../public/card/favicon-32x32.png'
-import { Form } from '@/components/Card details'
+import "./index.css";
+import { useState } from "react";
+import Image from "next/image";
+import cardLogo from "../../../public/card/card-logo.svg";
+import complete from "../../../public/card/icon-complete.svg";
+import cardIco from "../../../public/card/favicon-32x32.png";
+import { Form } from "@/components/Card details";
 import { Space_Grotesk } from "next/font/google";
 
 const spaceGrostesk = Space_Grotesk({
-    subsets: ['latin'],
-    weight : ['300' ,'500']
-})
+  subsets: ["latin"],
+  weight: ["300", "500"],
+});
 
 const CardDetails = () => {
-
-  const [cardDetails , setCardDetails] = useState({})
+  const [cardDetails, setCardDetails] = useState({});
+  const [submitted, setSubmitted] = useState(false);
 
   return (
-    <div className={`flex flex-col md:flex-row h-screen w-screen ${spaceGrostesk.className}`}>
-      <div className='background-colored w-full lg:w-1/3 relative'>
-      {/* ---caard front--- */}
-        <div className='card card-front mt-[10%] ml-[20%] flex flex-col justify-between p-6'>
-           <div>
-             <Image src={cardLogo} alt='cardLogo' />
-             {/* <Image src={cardIco} alt='cardIco' /> */}
-           </div>
-           <div >
-             <h1 className='font-bold text-sm md:text-xl tracking-widest'>
-             {`${cardDetails?.cardNumber ? cardDetails?.cardNumber : ' XXXX XXXX XXXX XXXX'}`}
-             </h1>
-             <div className='flex items-center justify-between mt-3'>
-              <p className='uppercase'>
-              {`${cardDetails?.cardHolderName ? cardDetails?.cardHolderName : 'Card Holder Name'}`}
+    <div
+      className={`flex flex-col md:flex-row h-screen w-screen ${spaceGrostesk.className}`}
+    >
+      <div className="background-colored w-full lg:w-1/3 relative">
+        {/* ---caard front--- */}
+        <div className="card card-front mt-[10%] ml-[20%] flex flex-col justify-between p-6">
+          <div>
+            <Image src={cardLogo} alt="cardLogo" />
+            {/* <Image src={cardIco} alt='cardIco' /> */}
+          </div>
+          <div>
+            <h1 className="font-bold text-sm md:text-xl tracking-widest">
+              {`${
+                cardDetails?.cardNumber
+                  ? cardDetails?.cardNumber
+                  : " XXXX XXXX XXXX XXXX"
+              }`}
+            </h1>
+            <div className="flex items-center justify-between mt-3">
+              <p className="uppercase">
+                {`${
+                  cardDetails?.cardHolderName
+                    ? cardDetails?.cardHolderName
+                    : "Card Holder Name"
+                }`}
               </p>
-              <p>{`${cardDetails?.month ? cardDetails?.month : 'MM'} / ${cardDetails?.year ? cardDetails?.year : 'YY'} `}</p>
-             </div>
-           </div>
-        </div> 
+              <p>{`${cardDetails?.month ? cardDetails?.month : "MM"} / ${
+                cardDetails?.year ? cardDetails?.year : "YY"
+              } `}</p>
+            </div>
+          </div>
+        </div>
 
-      {/* ---caard back--- */}
-        <div className='card card-back mt-[7%] ml-[35%]'>
-           <div className='cvv'>{`${cardDetails?.cvv ? cardDetails?.cvv : 'XXX'}`}</div>
+        {/* ---caard back--- */}
+        <div className="card card-back mt-[7%] ml-[35%]">
+          <div className="cvv">{`${
+            cardDetails?.cvv ? cardDetails?.cvv : "XXX"
+          }`}</div>
         </div>
       </div>
-      <div className='flex justify-center items-center flex-1 p-4  md:mt-0'>
-        <Form setCardDetails={setCardDetails} cardDetails={cardDetails} />
+      <div className="flex justify-center items-center flex-1 p-4  md:mt-0">
+        {!submitted ? (
+          <Form
+            setCardDetails={setCardDetails}
+            cardDetails={cardDetails}
+            onSuccess={() => setSubmitted(true)}
+          />
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <Image src={complete} className="complete-icon" />
+
+            <h1 className="text-[2rem] my-5 ">Thank You !</h1>
+            <p className=" mb-2 text-gray-400">We've added your card details.</p>
+            <button type="submit" className="btn" onClick={() => setSubmitted(false)}>
+              Continue
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardDetails
+export default CardDetails;
--- src/components/Card details/Form.js
+++ src/components/Card details/Form.js
@@ -1,33 +1,51 @@
 import { Space_Grotesk } from "next/font/google";
 
 const spaceGrostesk = Space_Grotesk({
-    subsets: ['latin'],
-    weight : ['300' ,'500']
-})
+  subsets: ["latin"],
+  weight: ["300", "500"],
+});
 
-const Form = ({ setCardDetails= ()=>{} , cardDetails={} }) => {
+const Form = ({
+  setCardDetails = () => {},
+  cardDetails = {},
+  onSuccess = () => {},
+}) => {
+  const handleChange = (key, value) => {
+    const temp = {
+      ...cardDetails,
+    };
 
-    const handleChange = (key , value) => {
-        const temp = {
-            ...cardDetails,
-        }
-
-        temp[key] = value
-
-        setCardDetails(temp)
+    if (key === "cardHolderName") {
+      temp[key] = value.replace(/[^a-zA-Z\s]/g, "");
+    } else if (key === "cardNumber") {
+      temp[key] = value.replace(/(\d{4})(?=\d)/g, "$1 ");
+    } else if (["month", "year", "cvv"].includes(key)) {
+      temp[key] = value.replace(/\D/g, "");
+    } else {
+      temp[key] = value;
     }
 
+    setCardDetails(temp);
+  };
 
   return (
     <div className={`lg:w-1/2 md:2/3 w-full ${spaceGrostesk.className}`}>
-      <form className="">
+      <form
+        className=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSuccess();
+        }}
+      >
         <div className="relative w-full">
           <label className="block">Card Holder Name</label>
           <input
             type="text"
+            required
             placeholder="Card Holder Name"
             className="form-input w-full"
-            onChange={(e) => handleChange('cardHolderName' , e.target.value)}
+            value={cardDetails?.cardHolderName}
+            onChange={(e) => handleChange("cardHolderName", e.target.value)}
           />
         </div>
 
@@ -35,10 +53,12 @@ const Form = ({ setCardDetails= ()=>{} , cardDetails={} }) => {
           <label className="block">Card Number</label>
           <input
             type="text"
+            required
+            value={cardDetails?.cardNumber}
             placeholder="XXXX XXXX XXXX XXXX"
+            maxLength={19}
             className="form-input w-full"
-            onChange={(e) => handleChange('cardNumber' , e.target.value)}
-
+            onChange={(e) => handleChange("cardNumber", e.target.value)}
           />
         </div>
 
@@ -46,25 +66,44 @@ const Form = ({ setCardDetails= ()=>{} , cardDetails={} }) => {
           <div className="w-full">
             <label>Expiry Date (MM / YY)</label>
             <div className="flex items-center gap-3">
-              <input type="text" placeholder="MM" className="form-input w-1/2"
-            onChange={(e) => handleChange('month' , e.target.value)}
-                />
-              <input type="text" placeholder="YYYY" className="form-input w-1/2"
-            onChange={(e) => handleChange('year' , e.target.value)}
-              
-               />
+              <input
+                type="text"
+                placeholder="MM"
+                value={cardDetails?.month}
+                maxLength={2}
+                required
+                className="form-input w-1/2"
+                onChange={(e) => handleChange("month", e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="YY"
+                value={cardDetails?.year}
+                maxLength={2}
+                required
+                className="form-input w-1/2"
+                onChange={(e) => handleChange("year", e.target.value)}
+              />
             </div>
           </div>
           <div className="w-full">
             <label className="block">CVV / CVC</label>
-            <input type="text" placeholder="e.g 123" className="form-input w-full" 
-            onChange={(e) => handleChange('cvv' , e.target.value)}
-             />
+            <input
+              type="text"
+              value={cardDetails?.cvv}
+              placeholder="e.g 123"
+              maxLength={3}
+              required
+              className="form-input w-full"
+              onChange={(e) => handleChange("cvv", e.target.value)}
+            />
           </div>
         </div>
 
         <div className="mt-4">
-            <button type='submit' className="btn">Continue</button>
+          <button type="submit" className="btn">
+            Continue
+          </button>
         </div>
       </form>
     </div>
