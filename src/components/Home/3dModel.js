0eff9981041e66b61b33b4439e823935ff70a93c
Sumit134Coder <mehrasumit356@gmail.com>
2023-07-19 09:10:06 +0530

    update : suspense fallback

--- src/components/Home/3dModel.js
+++ src/components/Home/3dModel.js
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Spline from '@splinetool/react-spline';
-
+import React , { Suspense } from "react";
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 const Model = () => {
   return (
-    <div className='h-[100vh]'>
+    <div className="h-[100vh]">
+      <Suspense
+        fallback={<div className="text-lg text-white font-bold bg-red-500">Loading...</div>}
+      >
         <Spline scene="https://prod.spline.design/TvQaIQ4VEymK3xxv/scene.splinecode" />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default Model
+export default Model;
