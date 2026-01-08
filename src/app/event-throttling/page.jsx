fc4a2429b82729cfcb5cf588a63ca4afc5293921
sumit_mehra <sumit.mehra@netsolutions.com>
2026-01-08 22:18:42 +0530

    init : event page

---
+++ src/app/event-throttling/page.jsx
@@ -0,0 +1,14 @@
+
+
+export const metadata = {
+    title: 'Event throttling',
+    description: 'A example of event throttling in react using event for mouse pointer'
+}
+
+const EventThrottle = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default EventThrottle
